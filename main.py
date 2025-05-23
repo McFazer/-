ffcce5f2016516c361b1aacccc48fbdbb8bdba58
McFazer <sadilovanataly@gmail.com>
2025-05-23 18:51:46 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from number_utils import is_prime, factorial, digit_sum
+
+num = 7
+print(f"{num} простое?", is_prostoe(num))  
+print(f"Факториал {num}:", factorial(num))  
+print(f"Сумма цифр {num}:", digit_sum(num))  
